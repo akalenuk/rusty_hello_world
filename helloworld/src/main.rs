2c16b74397df4dc53671b0aad93c92bbca92ead5
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-08-28 19:34:55 +0300

    loops

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let xs = [1., 2., 3.];
+    for i in 0..xs.len() {
+        println!("{}", xs[i]);}
+    for x in &xs {
+        println!("{}", x);}
+    for x in xs.iter() {
+        println!("{}", x);}
 }
