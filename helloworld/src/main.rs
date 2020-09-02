80a4b2dac3a478b67a65a3f5b346552299e61d87
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-09-02 20:04:31 +0300

    dereferencing for conversion not for printing

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,12 +1,18 @@
 fn main() {
-    let xs = [1., 2., 3.];
-    for i in 0..xs.len() {
-        println!("{}", xs[i]);}
-    for x in &xs {
-        println!("{}", x);}
-    for x in xs.iter() {
-        println!("{}", x);}
-    for i in 1..(3+1) {
+	let xs = [1f64, 2f64, 3f64];
+	let is = [1i32, 2i32, 3i32];
+	for i in 0..xs.len() {
+		println!("{}", xs[i]);}
+	for x in &xs {
+		println!("{}", x);}
+	for x in xs.iter() {
+		println!("{}", x);}
+	for i in 1..(3+1) {
 		let x = i as f64;
-        println!("{}", x);}
+		println!("{}", x);}
+	for i in &is {
+		println!("{}", i);}
+	for i in &is {
+		let x = *i as f64;
+		println!("{}", x);}
 }
